3d9b51ad4833127d90dd485ab58813bd829cccf8
Timothy Curchod <timofeyc@hotmail.com>
2015-05-02 13:27:45 +1000

    Initial commit from the Angular Material starter clone

---
+++ app/src/paintings/PaintingController.js
@@ -0,0 +1,91 @@
+(function(){
+  angular.module('paintings')
+       .controller('PaintingController', [
+          'paintingService', '$mdSidenav', '$mdBottomSheet', '$log', '$q',
+          PaintingController
+       ]);
+  /**
+   * Main Controller for the Angular Material Starter App
+   * @param $scope
+   * @param $mdSidenav
+   * @param avatarsService
+   * @constructor
+   */
+  function PaintingController( paintingService, $mdSidenav, $mdBottomSheet, $log, $q) {
+    var self = this;
+
+    self.selected     = null;
+    self.paintings        = [ ];
+    self.selectPainting   = selectPainting;
+    self.toggleList   = togglePaintingsList;
+    self.share        = share;
+    // Load all registered paintings
+    paintingService
+          .loadAllPaintings()
+          .then( function(paintings) {
+            self.paintings    = [].concat(paintings);
+            console.log('paintings '+paintings.length);
+            self.selected = paintings[0];
+          });
+    // *********************************
+    // Internal methods
+    // *********************************
+
+    /**
+     * First hide the bottomsheet IF visible, then
+     * hide or Show the 'left' sideNav area
+     */
+    function togglePaintingsList() {
+      var pending = $mdBottomSheet.hide() || $q.when(true);
+
+      pending.then(function(){
+        $mdSidenav('left').toggle();
+      });
+    }
+
+    /**
+     * Select the current avatars
+     * @param menuId
+     */
+    function selectPainting ( painting ) {
+      self.selected = angular.isNumber(painting) ? $scope.paintings[painting] : painting;
+      self.toggleList();
+    }
+
+    /**
+     * Show the bottom sheet
+     */
+    function share($event) {
+        var painting = self.selected;
+
+        $mdBottomSheet.show({
+          parent: angular.element(document.getElementById('content')),
+          templateUrl: '/src/paintings/view/contactSheet.html',
+          controller: [ '$mdBottomSheet', PaintingSheetController],
+          controllerAs: "vm",
+          bindToController : true,
+          targetEvent: $event
+        }).then(function(clickedItem) {
+          clickedItem && $log.debug( clickedItem.name + ' clicked!');
+        });
+
+        /**
+         * Bottom Sheet controller for the Avatar Actions
+         */
+        function PaintingSheetController( $mdBottomSheet ) {
+          this.painting = painting;
+          this.items = [
+            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
+            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
+            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
+            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
+          ];
+          this.performAction = function(action) {
+            $mdBottomSheet.hide(action);
+          };
+        }
+    }
+
+  }
+
+})();
---
+++ app/src/paintings/Paintings.js
@@ -0,0 +1,8 @@
+(function(){
+  'use strict';
+
+  // Prepare the 'paintings' module for subsequent registration of controllers and delegates
+  angular.module('paintings', [ 'ngMaterial' ]);
+
+
+})();
